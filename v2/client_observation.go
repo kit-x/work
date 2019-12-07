6e02ad0cd61f4d03cb003879e168ca89ec49983a
zhaolion <zhaoliangsyn@gmail.com>
2019-12-07 21:14:37 +0800

    v2/client: add WorkerObservation setter & delete function

--- v2/client_observation.go
+++ v2/client_observation.go
@@ -1,9 +1,56 @@
 package work
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/go-redis/redis"
 	"github.com/pkg/errors"
 )
+
+// WorkerObservation represents the latest observation taken from a worker.
+// The observation indicates whether the worker is busy processing a job,
+// and if so, information about that job.
+type WorkerObservation struct {
+	WorkerID string `json:"worker_id"`
+	IsBusy   bool   `json:"is_busy"`
+
+	// If IsBusy:
+	JobName   string `json:"job_name"`
+	JobID     string `json:"job_id"`
+	StartedAt int64  `json:"started_at"`
+	ArgsJSON  string `json:"args_json"`
+	Checkin   string `json:"checkin"`
+	CheckinAt int64  `json:"checkin_at"`
+
+	// for test
+	heartbeat *WorkerPoolHeartbeat `json:"-"`
+}
+
+func newWorkerObservation(hash map[string]string) *WorkerObservation {
+	ob := &WorkerObservation{IsBusy: true}
+	ob.WorkerID = hash["worker_id"]
+	ob.JobName = hash["job_name"]
+	ob.JobID = hash["job_id"]
+	ob.StartedAt, _ = strconv.ParseInt(hash["started_at"], 10, 64)
+	ob.ArgsJSON = hash["args"]
+	ob.Checkin = hash["checkin"]
+	ob.CheckinAt, _ = strconv.ParseInt(hash["checkin_at"], 10, 64)
+
+	return ob
+}
+
+func (wo *WorkerObservation) ToRedis() map[string]interface{} {
+	return map[string]interface{}{
+		"worker_id":  wo.WorkerID,
+		"job_name":   wo.JobName,
+		"job_id":     wo.JobID,
+		"started_at": wo.StartedAt,
+		"args":       wo.ArgsJSON,
+		"checkin":    wo.Checkin,
+		"checkin_at": wo.CheckinAt,
+	}
+}
 
 // WorkerObservations returns all of the WorkerObservation's it finds for all worker pools' workers.
 func (client *Client) WorkerObservations() ([]*WorkerObservation, error) {
@@ -35,6 +82,28 @@ func (client *Client) WorkerObservations() ([]*WorkerObservation, error) {
 	}
 
 	return obs, nil
+}
+
+func (client *Client) SetWorkerObservation(ob *WorkerObservation) error {
+	if ob.WorkerID == "" {
+		return errors.New("invalid WorkerObservation with empty WorkerID")
+	}
+
+	key := client.keys.WorkerObservationKey(ob.WorkerID)
+	setOb := func(pipe redis.Pipeliner) error {
+		pipe.HMSet(key, ob.ToRedis())
+		pipe.Expire(key, time.Hour*24)
+		return nil
+	}
+	if _, err := client.conn.Pipelined(setOb); err != nil {
+		return errors.WithStack(err)
+	}
+
+	return nil
+}
+
+func (client *Client) DeleteWorkerObservation(workerID string) error {
+	return errors.WithStack(client.conn.Del(client.keys.WorkerObservationKey(workerID)).Err())
 }
 
 func (client *Client) getWorkerIDs() ([]string, error) {
